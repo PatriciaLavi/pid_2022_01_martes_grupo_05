4f9a60e04a1a0dd00b57ec1895b169856fc3edfc
PatriciaLavi <PatriciaLavi@gmail.com>
2022-04-19 18:45:53 -0500

    avance1.1regact

--- src/main/java/com/empresa/controller/DepartamentoController.java
+++ src/main/java/com/empresa/controller/DepartamentoController.java
@@ -2,21 +2,24 @@ package com.empresa.controller;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Departamento;
 import com.empresa.service.DepartamentoService;
 
-@Controller
+@RestController
 @RequestMapping("/rest/departamento")
 public class DepartamentoController {
 	@Autowired
@@ -32,13 +35,20 @@ public class DepartamentoController {
 	public ResponseEntity<HashMap<String, Object>> insertaDepartamento(@RequestBody Departamento obj){
 		HashMap<String, Object> salida = new HashMap<String, Object>();
 		try {
-			Departamento objSalida = service.insertaDepartamento(obj);
-			if(objSalida==null) {
+			List<Departamento> lstDepartamento = service.listadepartamentopornum(obj.getNumDepa());
+			if(CollectionUtils.isEmpty(lstDepartamento)) {
+				obj.setCodDepa(0);
+				Departamento objsalida=service.insertActulizaDepartamento(obj);		
+			if(objsalida==null) {
 				salida.put("MENSAJE", "Error al insertar");
 			}else{
 				salida.put("MENSAJE", "Registro correcto");
 		}
-		}catch (Exception e) {
+		}else {
+			salida.put("mensaje", "el nro" + obj.getNumDepa() + "ya existe");
+		}
+		}
+			catch (Exception e) {
 			salida.put("MENSAJE", "Error al insertar");
 		} 
 		
@@ -46,7 +56,39 @@ public class DepartamentoController {
 	}
 	
 
+	@PutMapping
+	@ResponseBody
+	public ResponseEntity<HashMap<String, Object>> actualizaDepartamento(@RequestBody Departamento obj){
+		HashMap<String, Object> salida=new HashMap<String, Object>();
+		try {
+			Optional<Departamento> optional= service.buscaPorCod(obj.getCodDepa());
+	        if(optional.isEmpty()) {
+		salida.put("mensaje", "No existe Departamento de COD:" + obj.getCodDepa());
 		
+		}else {
+			List<Departamento> lstDepartamento=service.listaDepartamentoporCodigodiferentealm(obj.getNumDepa(), obj.getCodDepa());
+			if(CollectionUtils.isEmpty(lstDepartamento)) {
+				Departamento objsalida=service.insertActulizaDepartamento(obj);
+				if(objsalida==null) {
+					
+					salida.put("mensaje", "error al actualizar");				
+				}else {
+					salida.put("mensaje", "Se actualizo correctamente");
+				}}else {
+			salida.put("mensaje", "El Nro de Departamento"+ obj.getNumDepa() +" ya existe.");
+		}
+	}			
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "error en actualizar");
+		}
+	
+	return ResponseEntity.ok(salida);
+	
+	
+	
+}
+
 	
 	
 }
--- src/main/java/com/empresa/repository/DepartamentoRepository.java
+++ src/main/java/com/empresa/repository/DepartamentoRepository.java
@@ -3,8 +3,9 @@ package com.empresa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.empresa.entity.Departamento;
 
@@ -12,11 +13,11 @@ import com.empresa.entity.Departamento;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
 	
-	//cree mi queri  vas a registrar por nro de habitacion el codigo de depa si va a ir obligatoriamente pero para registrar necesito otra variable
-	//si te das cuenta de alumno registro a cual de los compas? haber dejame probar 
+public abstract List<Departamento> findBynumDepa(int numDepa);
 	
-	//@Query("select e from tbdepartamento e where e.=:pnroDepa and e.codDepa <>:pidtbdepartamento")
-	//	public abstract List<Departamento> listaDepartamentoPornrohabDiferente(@Param("pnroDepai")int nroDep,@Param ("pidtbdepartamento")int codDepa);
+	
+	@Query("select e from Departamento e where e.numDepa=:pnumDepa and e.codDepa <>:pcodDepa")
+		public abstract List<Departamento> listaDepartamentoPornrohabDiferente(@Param("pnumDepa")int numDep,@Param ("pcodDepa")int codDepa);
 		
 		
 	
